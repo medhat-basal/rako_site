05c30d81b2acff1989e0c14115cb473dac042403
medhat wael basal <109469876+medhat-basal@users.noreply.github.com>
2024-11-23 14:46:30 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,76 @@
+
+    var mb = document.getElementById("menuBtn");
+    var sv = document.getElementById("sidenav");
+
+
+
+    sv.style.right = "-250px";
+    mb.onclick = function (){
+    if(sv.style.right == "-250px"){
+        sv.style.right = "0";    
+        }else{
+        sv.style.right = "-250px"; 
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
